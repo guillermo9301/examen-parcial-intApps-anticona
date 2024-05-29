ce23f3730f5bf15e8d681ed878a66c12ad6a5df5
guillermo9301 <guille.anticona@gmail.com>
2024-05-28 21:57:53 -0500

    initial commit

---
+++ test/probando.test.js
@@ -0,0 +1,12 @@
+const request = require('supertest');
+
+const app = require('../app');
+
+describe('Probando la ruta raiz', () => {
+    test('Deberia responder al metodo get', async () => {
+        const response = await request(app).get('/inicio');
+        expect(response.statusCode).toBe(200);
+        // Verifica el contenido del archivo HTML si es necesario
+        expect(response.text).toContain('<title>Restaurante las Parrilas</title>');
+    });
+});
